d0c15cd5b9fccf2b930c016aaa85c02b5c454bc0
deltagare <bildermejl76@gmail.com>
2022-03-30 11:21:51 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/App.java
@@ -0,0 +1,12 @@
+package se.lexicon;
+import static se.lexicon.StringLengths.findLength;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        //1. What is the length of the String: "Java" ?
+        findLength();
+
+    }
+}
